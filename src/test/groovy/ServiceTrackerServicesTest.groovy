bc948bf2c39315dfb980d6d197fb9b8691985a98
Debmalya biswas <debmalya.biswas@intelliant.net>
2014-10-21 18:52:35 +0530

    Modify the service for automatic data entry for status entity. When reception enter a car's data then automatically status will be created for that car.

--- src/test/groovy/ServiceTrackerServicesTest.groovy
+++ src/test/groovy/ServiceTrackerServicesTest.groovy
@@ -4,6 +4,8 @@ import org.moqui.entity.EntityValue
 import spock.lang.*
 import spock.lang.Specification
 
+import javax.swing.text.html.parser.Entity
+
 
 /**
  * Created by debmalya.biswas on 20/10/14.
@@ -34,9 +36,10 @@ class ServiceTrackerServicesTest extends Specification {
     def "Create car entry with Reception Entity service"(){
         when:
         def createEntry = ec.service.sync().name("tracker.TrackerServices.createReceptionEntity")
-                                           .parameters([CarNo : "2", Job : "PM(Free)", ServiceAdvisor : "Abhishek Bagchi", InTime : "2014-10-08 21:20:00.0", DriverOrOwner : "Owner"
-                                                       , Gift : "Yes", DropCar : "Yes", CustomerWaiting : "No"]).call()
-        EntityValue carCreated = ec.entity.makeFind("service.tracker.ReceptionEntity").condition("CarNo", "2").one()
+                                           .parameters([CarNo : "WB-02_M4", Job : "PM(Free)", ServiceAdvisor : "Abhishek Bagchi", InTime : "2014-10-08 21:20:00.0", DriverOrOwner : "Owner",
+                                                      CarStatus : "Reception" , Gift : "Yes", DropCar : "Yes", CustomerWaiting : "No"]).call()
+        EntityValue carCreated = ec.entity.makeFind("service.tracker.ReceptionEntity").condition("CarNo", "WB-02_M4").one()
+        EntityValue status = ec.entity.makeFind("service.tracker.CarNoAndStatus").condition("CarNo", "WB-02_M4").one()
         then:
         carCreated != null
         carCreated.Job == "PM(Free)"
@@ -45,12 +48,24 @@ class ServiceTrackerServicesTest extends Specification {
         carCreated.Gift == "Yes"
         carCreated.DropCar == "Yes"
         carCreated.CustomerWaiting == "No"
+        status.CarStatus == "Reception"
         cleanup:
         carCreated.delete()
-
-
-
+        status.delete()
     }
+
+   /* def "Test Entity ECA rule for Reception Entity service"(){
+        when:
+        def createEntry = ec.service.sync().name("tracker.TrackerServices.createReceptionEntity")
+                .parameters([CarNo : "frtc", Job : "PM(Free)", ServiceAdvisor : "Abhishek Bagchi", InTime : "2014-10-08 21:20:00.0", DriverOrOwner : "Owner"
+                , Gift : "Yes", CarStatus : "Reception", DropCar : "Yes", CustomerWaiting : "No"]).call()
+        EntityValue getStatus = ec.entity.makeFind("service.tracker.CarNoAndStatus").condition("CarNo", "frtc").one()
+        then:
+        getStatus != null
+        getStatus.CarStatus == "Reception"
+        cleanup:
+       getStatus.delete()
+    } */
  /*   def "delete Record"(){
         when:
         def deleteRecord  = ec.entity.makeFind("service.tracker.ReceptionEntity").condition("CarNo", "WB-02-H-8917").one()
