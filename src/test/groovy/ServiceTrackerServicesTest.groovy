d801d4576cad63df8a0159ab9f9b1c6f15f58795
Debmalya biswas <debmalya.biswas@intelliant.net>
2014-10-23 13:02:04 +0530

    Added a service and some extra field for creating data for service advisor and updating the car status

--- src/test/groovy/ServiceTrackerServicesTest.groovy
+++ src/test/groovy/ServiceTrackerServicesTest.groovy
@@ -4,8 +4,6 @@ import org.moqui.entity.EntityValue
 import spock.lang.*
 import spock.lang.Specification
 
-import javax.swing.text.html.parser.Entity
-
 
 /**
  * Created by debmalya.biswas on 20/10/14.
@@ -36,10 +34,10 @@ class ServiceTrackerServicesTest extends Specification {
     def "Create car entry with Reception Entity service"(){
         when:
         def createEntry = ec.service.sync().name("tracker.TrackerServices.createReceptionEntity")
-                                           .parameters([CarNo : "WB-02_M4", Job : "PM(Free)", ServiceAdvisor : "Abhishek Bagchi", InTime : "2014-10-08 21:20:00.0", DriverOrOwner : "Owner",
-                                                      CarStatus : "Reception" , Gift : "Yes", DropCar : "Yes", CustomerWaiting : "No"]).call()
-        EntityValue carCreated = ec.entity.makeFind("service.tracker.ReceptionEntity").condition("CarNo", "WB-02_M4").one()
-        EntityValue status = ec.entity.makeFind("service.tracker.CarNoAndStatus").condition("CarNo", "WB-02_M4").one()
+                .parameters([CarNo : "WB-02_M5", Job : "PM(Free)", ServiceAdvisor : "Abhishek Bagchi", InTime : "2014-10-08 21:20:00.0", DriverOrOwner : "Owner",
+                CarStatus : "Reception" , Gift : "Yes", DropCar : "Yes", CustomerWaiting : "No"]).call()
+        EntityValue carCreated = ec.entity.makeFind("service.tracker.ReceptionEntity").condition("CarNo", "WB-02_M5").one()
+        EntityValue status = ec.entity.makeFind("service.tracker.CarNoAndStatus").condition("CarNo", "WB-02_M5").one()
         then:
         carCreated != null
         carCreated.Job == "PM(Free)"
@@ -56,34 +54,52 @@ class ServiceTrackerServicesTest extends Specification {
     def "Test Advisor entity"(){
         when:
         def createEntry = ec.entity.makeValue("service.tracker.AdvisorEntry")
-                                   .setAll([CarNo : "WB-02-M", CustomerName : "Rahul", CustomerMobile : "9830984765", DriverName : "Roy",
-                                            DriverMobile : "9840857843", BeforeRoadTest : "Yes", AfterRoadTest : "Yes"]).create()
+                .setAll([CarNo : "WB-02-M", CustomerName : "Rahul", CustomerMobile : "9830984765", DriverName : "Roy",
+                DriverMobile : "9840857843", BeforeRoadTest : "Yes", AfterRoadTest : "Yes"]).create()
         EntityValue created = ec.entity.makeFind("service.tracker.AdvisorEntry").condition("CarNo", "WB-02-M").one()
         then:
         created != null
         cleanup:
         created.delete()
     }
-
-   /* def "Test Entity ECA rule for Reception Entity service"(){
+    def "Test create advisor Entry services"(){
         when:
         def createEntry = ec.service.sync().name("tracker.TrackerServices.createReceptionEntity")
-                .parameters([CarNo : "frtc", Job : "PM(Free)", ServiceAdvisor : "Abhishek Bagchi", InTime : "2014-10-08 21:20:00.0", DriverOrOwner : "Owner"
-                , Gift : "Yes", CarStatus : "Reception", DropCar : "Yes", CustomerWaiting : "No"]).call()
-        EntityValue getStatus = ec.entity.makeFind("service.tracker.CarNoAndStatus").condition("CarNo", "frtc").one()
+                .parameters([CarNo : "WB-04-002", Job : "PM(Free)", ServiceAdvisor : "Abhishek Bagchi", InTime : "2014-10-08 21:20:00.0", DriverOrOwner : "Owner",
+                CarStatus : "Reception" , Gift : "Yes", DropCar : "Yes", CustomerWaiting : "No"]).call()
+        def updateAdvisor = ec.service.sync().name("tracker.TrackerServices.createAdvisorEntry")
+                .parameters([CarNo : "WB-04-002", CustomerName : "Rahul", MobileNo : "9999999999", DriverName : "Roy",
+                DriverMobile : "9840857843", BeforeRoadTest : "Yes", AfterRoadTest : "Yes", CarStatus : "Service Advisor"]).call()
+        EntityValue created =  ec.entity.makeFind("service.tracker.AdvisorEntry").condition("CarNo", "WB-04-002").one()
+        EntityValue statuscreated =  ec.entity.makeFind("service.tracker.CarNoAndStatus").condition("CarNo", "WB-04-002").one()
         then:
-        getStatus != null
-        getStatus.CarStatus == "Reception"
+        created != null
+        statuscreated != null
+        statuscreated.CarStatus == "Service Advisor"
         cleanup:
-       getStatus.delete()
-    } */
- /*   def "delete Record"(){
-        when:
-        def deleteRecord  = ec.entity.makeFind("service.tracker.ReceptionEntity").condition("CarNo", "WB-02-H-8917").one()
-        deleteRecord.
-        def findRecord = ec.entity.makeFind("service.tracker.ReceptionEntity").condition("CarNo", "WB-02-H-8917").one()
-        then:
-        findRecord == null
-    } */
+        created.delete()
+        statuscreated.delete()
+    }
+
+    /*  def "Test Entity ECA rule for Reception Entity service"(){
+          when:
+          def createEntry = ec.service.sync().name("tracker.TrackerServices.createReceptionEntity")
+                  .parameters([CarNo : "frtc", Job : "PM(Free)", ServiceAdvisor : "Abhishek Bagchi", InTime : "2014-10-08 21:20:00.0", DriverOrOwner : "Owner"
+                  , Gift : "Yes", CarStatus : "Reception", DropCar : "Yes", CustomerWaiting : "No"]).call()
+          EntityValue getStatus = ec.entity.makeFind("service.tracker.CarNoAndStatus").condition("CarNo", "frtc").one()
+          then:
+          getStatus != null
+          getStatus.CarStatus == "Reception"
+          cleanup:
+          getStatus.delete()
+      }
+      /*   def "delete Record"(){
+          when:
+          def deleteRecord  = ec.entity.makeFind("service.tracker.ReceptionEntity").condition("CarNo", "WB-02-H-8917").one()
+          deleteRecord.
+                  def findRecord = ec.entity.makeFind("service.tracker.ReceptionEntity").condition("CarNo", "WB-02-H-8917").one()
+          then:
+          findRecord == null
+      } */
 
 }
